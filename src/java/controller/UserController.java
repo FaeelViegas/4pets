d71753839074179e99cb63f7721ad0dc21fb8332
unknown <victorviegas036@gmail.com>
2024-05-02 14:52:33 -0300

    cria redirecionamento para a pagina de cadastro de usuario

--- src/java/controller/UserController.java
+++ src/java/controller/UserController.java
@@ -8,14 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "UserController", urlPatterns = {"/login-page"})
+@WebServlet(name = "UserController", urlPatterns = {"/login-page", "/register-user"})
 public class UserController extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String url = request.getServletPath();
-        if(url.equals("/login-page")){
+        if (url.equals("/login-page")) {
             String path = "/WEB-INF/jsp/login-user.jsp";
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(path);
+            dispatcher.forward(request, response);
+        } else if (url.equals("/register-user")) {
+            String path = "/WEB-INF/jsp/register-user.jsp";
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(path);
             dispatcher.forward(request, response);
         }
---
+++ web/WEB-INF/jsp/register-user.jsp
@@ -0,0 +1,14 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <main>
+        <h1>Cadastro de usuario</h1>
+    </main>
+</body>
+</html>
