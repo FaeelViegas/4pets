2fe5c1f628c07be29ef049a79d9cae489338311c
unknown <victorviegas036@gmail.com>
2024-06-14 15:15:20 -0300

    cria card dos endereços do usuario dinamicamente

--- web/js/profile-page-details.js
+++ web/js/profile-page-details.js
@@ -20,6 +20,31 @@ function insertDataUser(userData) {
     document.getElementById('phone').value = userData.phone;
 }
 
+function createAddressCard(address) {
+    const card = document.createElement('li');
+    card.innerHTML = `
+        <div id="${address.idAddress}" class="address d-flex">
+            <h4>${address.street}</h4>
+            <span>Numero: ${address.number}, ${address.complement}</span>
+            <span>Bairro: ${address.neighborhood}</span>
+            <span>CEP ${address.cep} - ${address.city},${address.state}</span>
+            <div class="buttons-address">
+                <button class="btn btn-outline-danger">Excluir</button>
+            </div>
+        </div>
+    `;
+    return card;
+}
+
+function loadAddresses(addresses) {
+    const ul = document.querySelector('.address-list');
+
+    addresses.forEach(address => {
+        const card = createAddressCard(address);
+        ul.appendChild(card);
+    });
+}
+
 fetch('./user-data-logged')
     .then(response => {
         if (!response.ok) {
@@ -30,6 +55,21 @@ fetch('./user-data-logged')
     .then(data => {
         const user = data[0];
         insertDataUser(user);
+    })
+    .catch(error => {
+        console.error(error);
+    });
+
+
+fetch('./addresses')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao obter dados');
+        }
+        return response.json();
+    })
+    .then(data => {
+        loadAddresses(data);
     })
     .catch(error => {
         console.error(error);
